bc80fd8edabcf206228767fb0ffd568b66b43eec
Pavan Macharla <85890242+PavanMacharla@users.noreply.github.com>
2023-06-25 12:24:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,19 +31,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-// Your code
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
-// Your code
 
 
 /* FILE STORAGE */
